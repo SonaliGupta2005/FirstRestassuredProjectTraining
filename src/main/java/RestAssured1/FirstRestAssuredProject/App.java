e886367e4cda8555146e868ccdbb349def724678
sonali gupta <sonali.gupta6@ibm.com>
2022-05-17 21:10:08 +0530

    FirstRestProject

---
+++ src/main/java/RestAssured1/FirstRestAssuredProject/App.java
@@ -0,0 +1,13 @@
+package RestAssured1.FirstRestAssuredProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
